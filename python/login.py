7537affac3ad4222231814ad2d14c00b7f79404f
PFOD1998 <p.f.odonoghue@gmail.com>
2020-01-24 17:13:35 +0000

    coded functions to check if logged in and to check if a user has admin rights

--- python/login.py
+++ python/login.py
@@ -1,4 +1,6 @@
 from password import Password
+from python.databases.databaseQueries import select_all_with_conditions
+
 from os import environ
 from hashlib import sha256
 from time import time
@@ -9,14 +11,50 @@ def loggedIn():
     """
     Function to check if user is logged in or not. Should be run on every page
     which requires a user to be logged in to use.
+    @return authenticated -> boolean showing if already logged in or not
+            username -> string of this user's username, empty string if unsuccessful
     """
+    authenticated = False
+    username = ""
+    try:
+        cookie = SimpleCookie()
+        http_cookie_header = environ.get('HTTP_COOKIE')
+        if not http_cookie_header:
+            sid = sha256(repr(time()).encode()).hexdigest()
+            cookie['sid'] = sid
+        else:
+            cookie.load(http_cookie_header)
+            if 'sid' not in cookie:
+                sid = sha256(repr(time()).encode()).hexdigest()
+                cookie['sid'] = sid
+            else:
+                sid = cookie['sid'].value
+
+                session_store = open('sessions/sess_' + sid, writeback=True)
+                authenticated = session_store.get("authenticated")
+                username = session_store.get("username")
+
+    except IOError:
+        authenticated,username = False,""
+
+    return authenticated, username
 #end loggedIn
 
 def isAdmin():
     """
     Function to check if a (loggged in) user has admin rights
-    @returns true or false
+    @param username
+    @return True or False
     """
+
+    # assumption that role will be the string "admin" for admin rights
+    result = select_all_with_2_conditions("users","id",username,"role","admin")
+    if len(result) > 0:
+        # This user is an admin
+        return True
+    # not admin
+    return False
+
 #end isAdmin
 
 def tryLogIn():
