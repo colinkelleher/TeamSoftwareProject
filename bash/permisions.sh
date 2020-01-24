c0df756403c1ca6c884cb9372e0f616de1638d21
jonathan hanley <jonathanhanley1234@gmail.com>
2020-01-24 22:46:15 +0000

    Added bash files

--- bash/permisions.sh
+++ bash/permisions.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-#Change all permisions for directories
 cd ..
+
+#Change all permisions for directories
 echo "Chmoding directories"
 find . -type d -exec chmod 755 {} +
 
 #Change all permisions for python files
 echo "Chmoding python files"
 find . -iname "*.py" -exec chmod +x {} +
+
+#Change all permisions for bash files
+echo "Chmoding bash files"
+find . -iname "*.sh" -exec chmod +x {} +
