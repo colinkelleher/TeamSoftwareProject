ad5afa42abaf4c72e265fba2855d03fd3bacee3d
jonathan hanley <jonathanhanley1234@gmail.com>
2020-01-24 22:29:36 +0000

    Created api endpoint for updating the product location

--- api/update_product_location.py
+++ api/update_product_location.py
@@ -1,10 +1,42 @@
 #!/usr/bin/python3
+from cgi import FieldStorage
 try:
     from python.login import *
+    from python.update_product_location import update_location
 
 except:
-    from ..python.login import *
+    from login import *
+    from update_product_location import update_location
 
 print('Content-Type: text/html')
 print()
-print(loggedIn())
+message = {"updated" : False, "message" : ""}
+if loggedIn():
+    message["message"] = "Sorry, you don't seem to be logged in"
+else:
+    form_data = FieldStorage()
+    pid = form_data.getfirst("pid")
+    lid = form_data.getfirst("lid")
+    if pid and lid:
+        result = update_location(pid, lid)
+        if result:
+            message["message"] = "That was updated"
+        elif result == 0:
+            message["message"] = "Sorry, that location does not exist"
+
+        elif result == -1:
+            message["message"] = "Sorry, that product does not exist"
+
+        elif result == -2:
+            message["message"] = "Sorry, something went wrong"
+
+    elif not pid and not lid:
+        message["message"] = "Sorry, both the product ID and location ID are empty"
+
+    elif not pid:
+        message["message"] = "Sorry, the product ID is empty"
+
+    elif not lid:
+        message["message"] = "Sorry, the location ID is empty"
+
+print(str(message))
