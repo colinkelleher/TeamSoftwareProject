573ba89fa03775d95780cb5cf430b6f59110dc86
PFOD1998 <p.f.odonoghue@gmail.com>
2020-01-24 16:50:50 +0000

    added select function for queries with 2 conditions

--- python/databases/databaseQueries.py
+++ python/databases/databaseQueries.py
@@ -44,5 +44,26 @@ def select_all_with_conditions(table, column_name, value):
     return result
 
 
+def select_all_with_2_conditions(table, column_name1, value1, column_name2, value2):
+    """
+    This function will return the results from a select statement with a where clause
+
+    Arguments:
+        table -- Name of the table to query
+        column_name (1 & 2) -- The name of the respective column that is being compared
+        value (1 & 2) -- The respective value that is to be matched
+
+    Returns:
+        result -- List of tuples representing each row returned
+    """
+
+    sql = "SELECT * FROM %s WHERE %s = %s AND %s = %s" % (table, column_name1, value1, column_name2, value2)
+
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    return result
+
+
 if __name__ == "__main__":
     print(select_all_with_conditions("products", "location", "1"))
