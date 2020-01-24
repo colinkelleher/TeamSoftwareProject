4db994a3f1fd6422a74be4a697c7b10ecb427207
jonathan hanley <jonathanhanley1234@gmail.com>
2020-01-24 21:37:44 +0000

    Changed connect function to find correct path to database.db regardless of directory depth

--- python/databases/connectToDatabase.py
+++ python/databases/connectToDatabase.py
@@ -2,13 +2,23 @@ import sqlite3
 from os import path
 
 
-def connect(url="../../databases/database.db"):
+def connect(url="DEFAULT"):
     """
-    :param url: String --> path to the database to be connected to
+    :param url: String --> path to the database to be connected to. DEFAULT to connect to database.db
     :return: connection --> sqlite3.connect() obj
              cursor --> sqlite3.cursor obj
     :raises: FileNotFoundError if the path given does not point to a file
     """
+
+    if url == "DEFAULT":
+        url = "databases/database.db"
+        if not path.exists(url):
+            url = "../databases/database.db"
+            if not path.exists(url):
+                url = "../../databases/database.db"
+                if not path.exists(url):
+                    url = "../../../databases/database.db"
+
     if path.exists(url):
         connection = sqlite3.connect(url)
         cursor = connection.cursor()
