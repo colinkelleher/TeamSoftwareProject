87d7f5efcda5f6de7f4eca0aea2870a8ccd65d56
theLiamDel <117317853@umail.ucc.ie>
2020-01-28 18:08:19 +0000

    Added function to create a table of products from a given location id

--- python/htmlGenerators/viewProducts.py
+++ python/htmlGenerators/viewProducts.py
@@ -14,17 +14,37 @@ def create_table_of_locations():
     output = "<section>"
 
     for location in locations:
-        products_stored_in_location = select_all_with_conditions("products", "location", location[0])
 
-        output += "<table><tr><th>%s</th></tr>" % (location[1])
-        output += "<tr><th>Product:</th><th>Description</th></tr>"
-
-        for product in products_stored_in_location:
-            output += "<tr><td>%s</td><td>%s</td></tr>" % (product[1], product[2])
-
-        output += "</table>"
+        output += create_table_of_products_from_location(location[0], location[1])
 
     output += "</section>"
+
+    return output
+
+
+def create_table_of_products_from_location(location_id, location_name):
+    """
+    This function creates a table from the products stores in a location
+
+    Arguments:
+        location_id - int representing the id of the location
+        location_name - String representing the name of the location
+
+    Returns:
+        output - String containing the table of products
+    """
+
+    output = ""
+
+    products_stored_in_location = select_all_with_conditions("products", "location", location_id)
+
+    output += "<table><tr><th>%s</th></tr>" % location_name
+    output += "<tr><th>Product:</th><th>Description</th></tr>"
+
+    for product in products_stored_in_location:
+        output += "<tr><td>%s</td><td>%s</td></tr>" % (product[1], product[2])
+
+    output += "</table>"
 
     return output
 
