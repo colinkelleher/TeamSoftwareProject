eba18775cca9fb95362452906ccdfcaa9e449ccb
jonathan hanley <jonathanhanley1234@gmail.com>
2020-01-29 21:19:31 +0000

    Added page to view details of a product

---
+++ webfiles/viewProduct.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from cgi import FieldStorage
+from python.login import *
+from python.html_components import *
+
+print('Content-Type: text/html')
+print()
+print_head()
+print_nav()
+print_main()
