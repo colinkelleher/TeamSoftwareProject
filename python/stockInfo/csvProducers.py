7fae7dbaae6a1d4842be4a65bf922cdcbdbe7bb7
theLiamDel <117317853@umail.ucc.ie>
2020-01-30 16:49:28 +0000

    Created base csv file and function to log products being added to system

---
+++ python/stockInfo/csvProducers.py
@@ -0,0 +1,31 @@
+import csv
+import os
+
+
+def add_product_to_history(product_id, product_title, location_id):
+    """
+    Logs a product being added to the system in a csv file called product_history.csv
+
+    TODO Maybe pass arguments as some sort of object to make code cleaner?
+
+    Arguments:
+        product_id -- The id of the product
+        product_title -- The name of the product
+        location_id -- The id of the location of the object
+    """
+
+    fields = ["id", "title", "location_id"]
+
+    if not os.path.exists("output/product_history.csv"):
+        with open("output/product_history.csv", "w+", newline="") as file:
+            writer = csv.DictWriter(file, fields)
+            writer.writeheader()
+
+    with open("output/product_history.csv", "a", newline="") as file:
+        writer = csv.DictWriter(file, fields)
+        writer.writerow({"id": product_id, "title": product_title, "location_id": location_id})
+
+
+if __name__ == "__main__":
+
+    add_product_to_history(1, "Ham", 1)
