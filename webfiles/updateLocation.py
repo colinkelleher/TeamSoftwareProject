acb693faec6c00983337e3114bab42fa5244ca1a
Karol <00name0010@gmail.com>
2020-02-04 15:53:29 +0000

    Initial update product location page

---
+++ webfiles/updateLocation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+try: import fix_import
+except: pass
+
+from python.webpage_functions import print_html, get_html_template
+
+print_html('main.html', dict(main=get_html_template('updateLocation.html').safe_substitute()))
