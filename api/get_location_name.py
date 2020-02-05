8fb2078d9de7ee537ca17742764011f86c9345cc
Karol <00name0010@gmail.com>
2020-02-05 18:51:21 +0000

    Create get location name api

---
+++ api/get_location_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+try: import fix_import
+except: pass
+
+import os
+# Import to authenticate user, as well as form data stuff
+from python.webpage_functions import get_form_data, has_form_data
+from python.databases.databaseQueries import select_all_with_conditions
+
+output = dict()
+if has_form_data():
+    location = get_form_data('lid')
+    row = select_all_with_conditions('locations', 'id', location)
+    if len(row):
+        output['name'] = row[0][1]
+
+print('Content-Type: text/json')
+print()
+print(output)
+
