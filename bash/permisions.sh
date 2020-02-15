8285561acbcc0d633faebd0f4a69120aa411120e
Karol <00name0010@gmail.com>
2020-02-15 15:58:15 +0000

    Update permissions.sh to chmod html, js, css, and pngs too and commit updated permissions for python files

--- bash/permisions.sh
+++ bash/permisions.sh
@@ -2,17 +2,28 @@
 
 cd ..
 
+chmod 755 .
+
 #Change all permisions for directories
 echo "Chmoding directories"
 find . -type d -exec chmod 755 {} +
 
-#Change all permisions for python files
+#Change all permisions for python files in index.py, api, and webfiles
 echo "Chmoding python files"
-find . -iname "*.py" -exec chmod +x {} +
+find api -type f -iname "*.py" | xargs chmod 705
+find webfiles -type f -iname "*.py" | xargs chmod 705
+chmod 705 index.py
 
 #Change all permisions for bash files
 echo "Chmoding bash files"
-find . -iname "*.sh" -exec chmod +x {} +
+find . -type f -iname "*.sh" -exec chmod +x {} +
 
 #Fix database permissions so apis can write to it. The folder its in also needs to be writable
-find . -name '*.db' -exec sh -c 'chmod 757 $(dirname $1)' - {} \;
+echo "Chmoding database"
+find . -type f -name '*.db' -exec sh -c 'chmod 757 $(dirname $1)' - {} \;
+
+# Change all permissions for html, js, css, png 
+for i in {html,js,css,png}; do
+  echo "Chmoding $i files"
+	find . -type f -iname "*.$i" | xargs chmod 604
+done	
