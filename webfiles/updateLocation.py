2a9508d87665690294e659b2e69077b030789cd3
Colin Kelleher <117303363@umail.ucc.ie>
2020-02-17 18:32:43 +0000

    Update QR Scanner location on screen!

--- webfiles/updateLocation.py
+++ webfiles/updateLocation.py
@@ -3,5 +3,4 @@ try: import fix_import
 except: pass
 
 from python.webpage_functions import print_html, get_html_template
-
 print_html('main.html', dict(main=get_html_template('updateLocation.html').safe_substitute()))
