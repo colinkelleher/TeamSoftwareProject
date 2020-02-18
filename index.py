06e7015b77dc35953dcb74713894e0225513d7a8
Karol <00name0010@gmail.com>
2020-02-18 17:16:02 +0000

    Don't forget to print logincookie

--- index.py
+++ index.py
@@ -5,26 +5,25 @@ from python.htmlGenerators import viewProducts, nav
 from python.password import Password
 from python.login import tryLogIn
 
-
 # Either show login page or landing page
 if not user.logged_in:
-        if has_form_data():
-            # Page loading with incoming form data
-            # Try to log in with the data
-            login_id, password = get_form_data("id"), Password(get_form_data("password"))
-            logInCookie = tryLogIn(login_id, password)
-            if logInCookie:
-                # successful
-                cookie = logInCookie.copy()
-                #cookie = logInCookie
-                # print the main dashboard
-                print_main(viewProducts.create_table_of_locations())
-            else:
-                # unsuccessful - print login page with error message
-                print_html('login.html') #### Error message?
+    if has_form_data():
+        # Page loading with incoming form data
+        # Try to log in with the data
+        login_id, password = get_form_data("id"), Password(get_form_data("password"))
+        logInCookie = tryLogIn(login_id, password)
+        if logInCookie:
+            # successful
+            cookie = logInCookie.copy()
+            # cookie = logInCookie
+            # print the main dashboard
+            print_main(viewProducts.create_table_of_locations(), cookie=logInCookie)
         else:
-            # No data - Print log in page
-            print_html('login.html')
+            # unsuccessful - print login page with error message
+            print_html('login.html')  #### Error message?
+    else:
+        # No data - Print log in page
+        print_html('login.html')
 
 else:
     print_main(viewProducts.create_table_of_locations())
