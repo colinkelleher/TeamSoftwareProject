f0495e9e19bda196343bbfd7af919f2db2fbea93
Karol <00name0010@gmail.com>
2020-02-19 15:09:14 +0000

    Fix signup css url

--- webfiles/signup.py
+++ webfiles/signup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from python.webpage_functions import user, print_html, get_form_data, has_form_data
+from python.webpage_functions import print_html, get_form_data, has_form_data
 from python.password import Password
 from python.login import register
 
