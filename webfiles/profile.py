30e65393db90cb5e78e33878a60c1de9db173873
theLiamDel <117317853@umail.ucc.ie>
2020-02-20 14:02:03 +0000

    Added comments

--- webfiles/profile.py
+++ webfiles/profile.py
@@ -5,10 +5,13 @@ from python.user import get_user
 from python.databases.databaseQueries import select_all_with_conditions
 
 
+# Get the users email address, the key of the database
 user_email = get_user().email
 
+# use this key to search for the users info and get a dictionary of info
 user_info = select_all_with_conditions("users", "email", user_email)[0]
 
+# Print the html page with users info
 print_main('profile.html', {"image": user_info["image"], "firstName": user_info["fname"],
                             "secondName": user_info["lname"], "email": user_info["email"],
                             "role": user_info["role"]})
