60d7229ede85648151910feee4627783c8df2c8d
PFOD1998 <p.f.odonoghue@gmail.com>
2020-02-20 11:38:03 +0000

    Additions to signup

--- webfiles/signup.py
+++ webfiles/signup.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 
-from python.webpage_functions import print_html, get_form_data, has_form_data
+from python.webpage_functions import print_html, get_form_data, has_form_data, print_main, get_user
 from python.password import Password
 from python.login import register
 
-if has_form_data():
-    # collect data
-    fname, lname, email, pword = get_form_data("fname"), get_form_data("lname"), get_form_data("email"), Password(get_form_data("pword"))
-    registered = register(email, pword, fname, lname)
-    if registered:
-        ## Print "successfully registered" page
-        print_html()
+
+if not get_user().logged_in:
+    if has_form_data():
+        # collect data
+        fname, lname, email, pword = get_form_data("fname"), get_form_data("lname"), get_form_data("email"), Password(get_form_data("pword"))
+        #send to db
+        registered = register(email, pword, fname, lname)
+        # check if successfully registered
+        if registered:
+            ## Print "successfully registered" page
+            print_html("login.html")
+        else:
+            # print signup page
+            #### preferably with error message
+            print_html("signup.html")
     else:
         print_html("signup.html")
 else:
-    print_html("signup.html")
+    print_html('redirect.html', dict(url='index.py'))
