0ec7e4463edfe62a010a55576fe2bfb3ddfb530f
Karol <00name0010@gmail.com>
2020-02-29 19:41:27 +0000

    If there's no database.db just create a new one, don't raise an exception

--- python/databases/connectToDatabase.py
+++ python/databases/connectToDatabase.py
@@ -9,20 +9,15 @@ def connect(url="DEFAULT"):
     :param url: String --> path to the database to be connected to. DEFAULT to connect to database.db
     :return: connection --> sqlite3.connect() obj
              cursor --> sqlite3.cursor obj
-    :raises: FileNotFoundError if the path given does not point to a file
     """
 
     if url == "DEFAULT":
         url = get_abs_paths()['data_store'] + '/database.db'
 
-    if path.exists(url):
-        connection = sqlite3.connect(url)
-        cursor = connection.cursor()
+    connection = sqlite3.connect(url)
+    cursor = connection.cursor()
 
-        return connection, cursor
-
-    else:
-        FileNotFoundError("This is not a valid path")
+    return connection, cursor
 
 
 if __name__ == "__main__":
