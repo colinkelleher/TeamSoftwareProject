9da53845142e2abd050c8c61ffc898e92b3110bf
jonathan hanley <jonathanhanley1234@gmail.com>
2020-03-02 14:34:49 +0000

    Fixed bug showing admin if user has just become admin

--- webfiles/makeAdmin.py
+++ webfiles/makeAdmin.py
@@ -2,7 +2,10 @@
 
 from python.webpage_functions import print_html, get_form_data, has_form_data, print_main, get_user
 from python.databases.databaseQueries import select_all_with_conditions, update_user
-
+if get_user().logged_in:
+    if has_form_data():
+        user_id = get_form_data("users")
+        updated = update_user(int(user_id), "role", "1")
 userForm = ""
 users = select_all_with_conditions("users", "role", "0")
 if len(users) > 0:
